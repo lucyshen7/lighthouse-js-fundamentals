b22df38f194d5e88a04157e4180bbb24b64007e4
Lucy Shen <lucyshen77@gmail.com>
2021-11-10 00:19:26 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,5 @@
+// if Statements
+
 const temperature = 30;
 
 if (temperature < 0) {
@@ -9,3 +11,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// Logical operators
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+const raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
